4b0d17ba5472c3f113189c58b0b437bf0d5cbaa0
Mary Franks <maryfranks@gmail.com>
2018-07-23 18:11:14 -0400

    get red wines from api and save to file

--- lcbo-parser.js
+++ lcbo-parser.js
@@ -3,14 +3,18 @@ const fs = require('fs');
 
 const baseUrl = "http://lcboapi.com/products"
 
-
 axios.get(baseUrl + '?where=is_vqa')
-  .then(response => {
-    // console.log(response.data);
-    let jsonData = JSON.stringify(response.data)
-    fs.writeFileSync('wines.json', jsonData);
-    console.log("file created");
-  })
-  .catch(error => {
-    console.log(error);
-  });
+     .then(response => {
+        let results = Object.values(response.data.result)
+        console.log(typeof results);
+        let wines = results.filter(result => result.secondary_category = "Red Wine");
+
+        // console.log(wines[0]);
+        // stringify the response to write it to a file
+        let fileContent = JSON.stringify(wines)
+        fs.writeFileSync('wines.json', fileContent);
+        console.log("file created");
+     })
+     .catch(error => {
+        console.log(error);
+     });
