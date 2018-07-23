f69732ac6e4ee8145d4482cba5ade4e2ad44ae54
Mary Franks <maryfranks@gmail.com>
2018-07-23 18:44:42 -0400

    refactor: return red and white wines separately from api

--- lcbo-parser.js
+++ lcbo-parser.js
@@ -6,14 +6,19 @@ const baseUrl = "http://lcboapi.com/products"
 axios.get(baseUrl + '?where=is_vqa')
      .then(response => {
         let results = Object.values(response.data.result)
-        console.log(typeof results);
-        let wines = results.filter(result => result.secondary_category = "Red Wine");
+        console.log("Total number of wines is " + results.length);
+        let redWines = results.filter(result => result.secondary_category === "Red Wine");
+        let whiteWines = results.filter(result => result.secondary_category === "White Wine");
+        let redWineTotal = redWines.length;
+        let whiteWineTotal = whiteWines.length;
+        console.log("Red Wines: " + redWineTotal);
+        console.log("White Wines: " + whiteWineTotal);
 
-        // console.log(wines[0]);
         // stringify the response to write it to a file
-        let fileContent = JSON.stringify(wines)
+        let fileContent = JSON.stringify(redWines)
         fs.writeFileSync('wines.json', fileContent);
-        console.log("file created");
+        console.log("file created, " + redWineTotal + " red wines written to file");
+
      })
      .catch(error => {
         console.log(error);
