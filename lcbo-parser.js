4eb7dbe35c8d6af5bb20b293236f32ce9da4a86a
Mary Franks <maryfranks@gmail.com>
2018-07-23 18:49:22 -0400

    save only the wines to the file

--- lcbo-parser.js
+++ lcbo-parser.js
@@ -3,21 +3,17 @@ const fs = require('fs');
 
 const baseUrl = "http://lcboapi.com/products"
 
-axios.get(baseUrl + '?where=is_vqa')
+axios.get(baseUrl)
      .then(response => {
         let results = Object.values(response.data.result)
-        console.log("Total number of wines is " + results.length);
-        let redWines = results.filter(result => result.secondary_category === "Red Wine");
-        let whiteWines = results.filter(result => result.secondary_category === "White Wine");
-        let redWineTotal = redWines.length;
-        let whiteWineTotal = whiteWines.length;
-        console.log("Red Wines: " + redWineTotal);
-        console.log("White Wines: " + whiteWineTotal);
+        console.log("Total number of results is " + results.length);
+        let wines = results.filter(result => result.primary_category === "Wine");
+        let wineTotal = wines.length;
 
         // stringify the response to write it to a file
-        let fileContent = JSON.stringify(redWines)
+        let fileContent = JSON.stringify(wines)
         fs.writeFileSync('wines.json', fileContent);
-        console.log("file created, " + redWineTotal + " red wines written to file");
+        console.log("file created, " + wineTotal + " wines written to file");
 
      })
      .catch(error => {
