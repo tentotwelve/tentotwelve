18c04661f4dcff3f91c5045b2043cd687d7dd36b
Mary Franks <maryfranks@gmail.com>
2018-07-25 07:43:59 -0400

    loop through multiple pages of results and save to file

--- lcbo-parser.js
+++ lcbo-parser.js
@@ -2,9 +2,14 @@ const axios = require('axios');
 const fs = require('fs');
 
 const baseUrl = "http://lcboapi.com/products"
-const testUrl = "?where=is_vqa&where=has_value_added_promotion&per_page=55&page="
+const testUrl = "?where=is_vqa&where=has_value_added_promotion&per_page=40&page="
 let pageNumber = 1
 const file = "./wines.json"
+
+if (fs.statSync(file)) {
+  fs.unlinkSync(file);
+  console.log(`${file} deleted`);
+}
 
 axios.get(baseUrl + testUrl + pageNumber)
      .then(getWines)
@@ -17,17 +22,16 @@ function getWines(response) {
   console.log("Total number of results is " + results.length);
   let wines = results.filter(result => result.primary_category === "Wine");
   writeJSONFile(wines);
+  if (response.data.pager.is_final_page === false) {
+    let pageNumber = response.data.pager.next_page
+    console.log(`page: ${pageNumber}`);
+    axios.get(baseUrl + testUrl + pageNumber).then(getWines).catch(error => { console.log(error); });
+  }
 }
 
 function writeJSONFile(wines) {
   let fileContent = JSON.stringify(wines)
   let wineTotal = wines.length;
-
-  if (fs.statSync(file)) {
-    fs.unlinkSync(file);
-    console.log(`${file} deleted`);
-  }
-
   fs.appendFileSync(file, fileContent);
   console.log(`${file} created, ${wineTotal} wines written to file`);
 }
