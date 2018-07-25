9cef4f66f5e5729f5d29db46a5a96f4cee566fa9
Mary Franks <maryfranks@gmail.com>
2018-07-25 06:46:42 -0400

    refactor: move get wines to own method

--- lcbo-parser.js
+++ lcbo-parser.js
@@ -2,17 +2,20 @@ const axios = require('axios');
 const fs = require('fs');
 
 const baseUrl = "http://lcboapi.com/products"
+const testUrl = "?where=is_vqa&where=has_value_added_promotion&per_page=55&page="
 
 axios.get(baseUrl)
-     .then(response => {
-        let results = Object.values(response.data.result)
-        console.log("Total number of results is " + results.length);
-        let wines = results.filter(result => result.primary_category === "Wine");
-        writeJSONFile(wines);
-     })
+     .then(getWines)
      .catch(error => {
         console.log(error);
      });
+
+function getWines(response) {
+  let results = Object.values(response.data.result)
+  console.log("Total number of results is " + results.length);
+  let wines = results.filter(result => result.primary_category === "Wine");
+  writeJSONFile(wines);
+}
 
 function writeJSONFile(wines) {
   let fileContent = JSON.stringify(wines)
